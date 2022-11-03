65ac930a0c6d0385a6995f42e03ef53f0c840526
Iyida Clement Ikechukwu <iyidaclem@gmail.com>
2022-11-03 12:55:15 +0000

    Dapp working

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { Web3Button, useAccount } from '@web3modal/react';
+import "./App.css"
 function App() {
+  const { account } = useAccount()
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      {account.isConnected ? <h1>{account.address}</h1> : null}
+      <Web3Button />
     </div>
   );
 }
