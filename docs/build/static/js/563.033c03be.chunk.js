c88e08b24a8d878462d613916903d1d23cdf1b0c
Iyida Clement Ikechukwu <iyidaclem@gmail.com>
2022-11-11 16:09:04 +0100

    Saved

--- docs/build/static/js/563.033c03be.chunk.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunklearnable_wallet_connect=self.webpackChunklearnable_wallet_connect||[]).push([[563],{6563:function(){}}]);
--- docs/build/static/js/601.c5614404.chunk.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunklearnable_wallet_connect=self.webpackChunklearnable_wallet_connect||[]).push([[601],{6601:function(){}}]);
